5ab62fce40cdef1528322ffea8c096f7dd625f2f
Arkadiusz Nitka <ark.nitka@gmail.com>
2021-12-19 17:30:18 +0100

    static files change

--- commerce/settings.py
+++ commerce/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+# Note: Replace 'supersecure.codes' with your domain
+STATIC_ROOT = "/var/www/artforge.pl/static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 from django.contrib.messages import constants as messages
 
