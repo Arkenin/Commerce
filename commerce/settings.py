36574ed77366715179707584700443a156b10641
Arkadiusz Nitka <ark.nitka@gmail.com>
2022-03-21 01:31:09 +0100

    Add readme and Add production config for gunicorn

--- commerce/settings.py
+++ commerce/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-# Note: Replace 'supersecure.codes' with your domain
-STATIC_ROOT = "/var/www/artforge.pl/static"
-STATICFILES_DIRS = [BASE_DIR + "/static"]
+
+STATIC_ROOT = "/var/www/artforge.pl/static/"
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "/static/"),]
+
 
 from django.contrib.messages import constants as messages
 
